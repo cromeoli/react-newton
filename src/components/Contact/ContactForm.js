7468fac7304642f24857319fc6f60f87c84813e2
Christian <cromoli1808@g.educaand.es>
2023-01-17 08:39:22 +0100

    contacto validado tambien

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -6,10 +6,64 @@ function ContactForm(props){
 
     const [index, setIndex] = useState(0);
 
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [textArea, setTextArea] = useState('');
+
+    const [emailError, setEmailError] = useState('');
+    const [usernameError, setUsernameError] = useState('');
+    const [textAreaError, setTextAreaError] = useState('');
+
+    const [success, setSuccess] = useState('');
+
     function handleOnCut(){
         console.log("focused")
         console.log(possibleMessages[1])
         setIndex(Math.floor(Math.random() * 4))
+    }
+
+    const handleBlur = (event) => {
+        const { name, value } = event.target;
+        switch (name) {
+            case 'email':
+                if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+                    console.log(email)
+                    setEmailError('Email no válido');
+                } else {
+                    setEmailError('');
+                }
+                break;
+            case 'username':
+                if (!/^[a-zA-Z0-9_]{3,15}$/.test(value)) {
+                    console.log(username)
+                    setUsernameError('Usuario debe contener de 3 a 15 caracteres y solo puede incluir "_"');
+                } else {
+                    setUsernameError('');
+                }
+                break;
+            case 'textarea':
+                if (value.length > 200) {
+                    setTextAreaError('El mensaje es demasiado largo...');
+                } else {
+                    setTextAreaError('');
+                }
+                break;
+
+            default:
+                break;
+        }
+    };
+
+    function sendMessage(){
+        if (!usernameError && !emailError && !textAreaError) {
+            localStorage.setItem('contact', textArea);
+
+            setSuccess('Mensaje enviado')
+            setTimeout(() => {
+                setSuccess('')
+            }, 1000);
+        }
+
     }
 
 
@@ -21,19 +75,37 @@ function ContactForm(props){
             <form className="contactForm">
                 <input className="formInputBox contactInput"
                        placeholder="Nickname"
+                       defaultValue={localStorage.getItem('username')}
+                       name="username"
+                       onBlur={handleBlur}
+                       onChange={(event) => setUsername(event.target.value)}
                 />
                 <input className="formInputBox contactInput"
                        placeholder="Email"
+                       defaultValue={localStorage.getItem('email')}
+                       name="email"
+                       onBlur={handleBlur}
+                       onChange={(event) => setEmail(event.target.value)}
                 />
 
                 <textarea className={`contactTextArea resetTextArea `}
                           placeholder={possibleMessages[index]}
+                          name="textarea"
                           onFocus={handleOnCut}
+                          onBlur={handleBlur}
+                          onChange={(event) => setTextArea(event.target.value)}
                 />
-                <a className="donateButton contactButton">
+                <a className="donateButton contactButton"
+                   onClick={sendMessage}
+                >
                     Send
                 </a>
             </form>
+
+            <p className="registerSuccess">{success}</p>
+            <p className="registerErrorText">{usernameError}</p>
+            <p className="registerErrorText">{emailError}</p>
+            <p className="registerErrorText">{textAreaError}</p>
         </div>
     );
 }
